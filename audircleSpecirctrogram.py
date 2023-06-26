294edb0c7a05da30aaf71028e5f197a386a67bfa
Yoshiaki Horiike <77383370+yoshiysoh@users.noreply.github.com>
2023-06-26 10:40:43 +0200

    update doc string

--- audircleSpecirctrogram.py
+++ audircleSpecirctrogram.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """
-Plot the live microphone signal(s) with Matplotlib.
+Plot the live microphone signal(s) with PyQtGraph.
 
 Original code is from example of code of python-sounddevice
 https://github.com/spatialaudio/python-sounddevice/blob/f148dc64c9eb043adcf8de3b34890e8f3df71636/examples/plot_input.py
